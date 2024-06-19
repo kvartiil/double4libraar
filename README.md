0af6f8db13575015c84f2a71592c207381d0e33e
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-06-19 15:07:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# double4libraar
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kvartiil/double4libraar)
---
+++ projects/elujou-lib/src/lib/tabx/tabx.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-tab',
+  standalone: true,
+  templateUrl: './tabx.component.html',
+  styleUrls: ['./tabx.component.scss']
+})
+export class TabComponent {//} implements OnInit {
+  //myForm: FormGroup
+  //constructor(private fb: FormBuilder) {
+  //  this.buildForm()
+  // }
+
+  //buildForm(){
+  //  this.myForm = this.fb.group({
+  //    buyInAmount: ['', Validators.required],
+  //    buyInGrams: ['', Validators.required]
+  //  })
+  //}
+
+
+  //ngOnInit() {
+  //}
+
+}
